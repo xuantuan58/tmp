3b84f6aa346c311302bde041efbe05f96ce70599
Tuan Bui <xuantuan58@gmail.com>
2016-07-02 18:22:26 +0700

    Update Test.java

--- Test.java
+++ Test.java
@@ -3,5 +3,6 @@ package com.test.tuan;
 public class Test {
 	public static void main(String[] args) {
 		System.out.print("this. s is unable to merge what is the hell text is required");
+		System.out.print("this new request");
 	}
 }
