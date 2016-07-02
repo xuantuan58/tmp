0e5444b3429fda90369ac5306188a11e95b89569
Tuan Bui <xuantuan58@gmail.com>
2016-07-02 19:01:13 +0700

    Update a.java

--- a.java
+++ a.java
@@ -1,7 +1,8 @@
 package com.test.all
 public class Test {
 	public static void main (String[] args) {
-		System.out.print("this is a text");
+		System.out.print("this is a text a more text");
+		System.out.print("abc");
 	}
 }
 
