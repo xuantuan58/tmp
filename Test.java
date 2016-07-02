df3c3a5cee20aad3cf61ebd69ff281dcc2869337
tuan.bui <xuantuan58@gmail.com>
2016-07-02 18:50:56 +0700

    merged my stash

--- Test.java
+++ Test.java
@@ -4,5 +4,6 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.print("this. s is unable to merge what is the hell text is required");
 		System.out.print("this new request");
+		System.out.print("this is a change");
 	}
 }
