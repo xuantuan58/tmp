31a75376139a338ebd94da32d37f019025175196
tuan.bui <xuantuan58@gmail.com>
2016-07-02 19:15:48 +0700

    yeah

--- Test.java
+++ Test.java
@@ -5,5 +5,7 @@ public class Test {
 		System.out.print("this. s is unable to merge what is the hell text is required");
 		System.out.print("this new request");
 		System.out.print("this is a change change many times");
+		System.out.print("this is a abc change");
+		System.out.print("hehe!!!");
 	}
 }
--- a.java
+++ a.java
@@ -1,7 +1,7 @@
 package com.test.all
 public class Test {
 	public static void main (String[] args) {
-		System.out.print("this is a text");
+		 System.out.print("this is a I want to change here text");
 	}
 }
 
