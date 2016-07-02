596c9a81092b6bd12db3d6b0fc8b5b7afca28e5d
Tuan Bui <xuantuan58@gmail.com>
2016-07-02 19:00:13 +0700

    Update Test.java

--- Test.java
+++ Test.java
@@ -4,6 +4,6 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.print("this. s is unable to merge what is the hell text is required");
 		System.out.print("this new request");
-		System.out.print("this is a change");
+		System.out.print("this is a change change many times");
 	}
 }
